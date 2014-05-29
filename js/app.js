cac9d07e45621e9240a74a26c226345cd0d98f55
Samantha <samantha.stallings1@gmail.com>
2014-05-29 12:09:13 -0700

    new toggling function
    function changes li class from complete to incomplete when clicked on

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 //******Functions*******//
 
-//**Submit User Input**//
 
 $(document).ready(function(){
 
-
+//enters user input when the add button is clicked
 function buttonSubmit(){
 userInput = document.getElementById('user-input').value;
 	if(userInput === '' || userInput === " "){
 		return;
 	} else {
-		todo = '<li class ="item-container incomplete">' + '<p class="to-do-item">' + userInput + '</p>' + '<span class="change-status">' +'</span>' + '</li>';
+		todo = '<li class ="item-container incomplete"><p class="to-do-item">' + userInput + '</p><span id="action" class="change-status"></span></li>';
 		$(todo).prependTo('.list').slideDown('350');
 		document.getElementById('user-input').value = '';
 
 		}
 }
 
-
+//enters user input when "enter" key is pressed
 function enterSubmit() {
     document.getElementById("user-input").onkeypress = function(event) {
         if(event.keyCode == 13) {
@@ -27,14 +26,31 @@ function enterSubmit() {
     };
 }
 
-function emptyList (){
+//clears entire ul
+function refreshList (){
 	$(".list").empty();
 }
+
+//toggle class on click from being complete to incomplete and change icon from square to check simultaneously
+$(".item-container").click(function() {
+    $(this).toggleClass("complete").toggleClass("incomplete");
+    $(this).children("#action").toggleClass("change-status").toggleClass("complete-icon");
+})
+
+//function to delete items on list that have been completed
+function deleteComplete() {
+    $(this).closest('.page-wrap').find('.list').children('.complete').slideUp(function() {
+    $(this).remove();
+    });
+}
+
 
 //******Event Handlers******//
 $('#user-input').keydown(enterSubmit);
 $('.submit').click(buttonSubmit);
-$('#refresh').click(emptyList);
+$('#refresh').click(refreshList);
 $('.list').sortable({ axis: "y" });
-
+$('#trash').click(deleteComplete);
 });
+
+
