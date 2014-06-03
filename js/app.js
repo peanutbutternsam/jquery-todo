a7f50cfb0f7d287c4989acd9064ef757cef3c990
Samantha <samantha.stallings1@gmail.com>
2014-06-02 21:14:11 -0700

    added functionality to delete button
    added mouseenter and mouseleave event handlers for trash can to change
    class prior to delete click event

--- js/app.js
+++ js/app.js
@@ -3,54 +3,93 @@
 
 $(document).ready(function(){
 
-//enters user input when the add button is clicked
-function buttonSubmit(){
-userInput = document.getElementById('user-input').value;
-	if(userInput === '' || userInput === " "){
-		return;
-	} else {
-		todo = '<li class ="item-container incomplete"><p class="to-do-item">' + userInput + '</p><span id="action" class="change-status"></span></li>';
-		$(todo).prependTo('.list').slideDown('350');
-		document.getElementById('user-input').value = '';
+	//enters user input when the add button is clicked
+	function addTodo(e){
 
-		}
-}
+		if (e.type == 'keydown' && e.keyCode !== 13) {
+			return;
+		} 
 
-//enters user input when "enter" key is pressed
-function enterSubmit() {
-    document.getElementById("user-input").onkeypress = function(event) {
-        if(event.keyCode == 13) {
-            event.preventDefault();
-            $('.submit').click();
-        }
-    };
-}
+		var $todo = $('<li class ="item-container incomplete"><span id="action" class="change-status"></span></li>'),
+			newVal = $('#user-input').val()
+			todoItem = $('<p class="to-do-item"></p>').append(newVal);
 
-//clears entire ul
-function refreshList (){
-	$(".list").empty();
-}
+		//look up the selector inside our li and append it in
+		$todo.append(todoItem);
 
-//toggle class on click from being complete to incomplete and change icon from square to check simultaneously
-$(".item-container").click(function() {
-    $(this).toggleClass("complete").toggleClass("incomplete");
-    $(this).children("#action").toggleClass("change-status").toggleClass("complete-icon");
-})
+		//prepend it to the list with a slidedown
+		$todo.prependTo($('.list')).slideDown('350');
+		
+		//bind new events
+		bindEvents($todo);
 
-//function to delete items on list that have been completed
-function deleteComplete() {
-    $(this).closest('.page-wrap').find('.list').children('.complete').slideUp(function() {
-    $(this).remove();
+		//clear out the text
+		clearInput();
+	}
+
+	//function to clear out text box after user input has been submit
+	function clearInput() {
+		$('#user-input').val('');
+	}
+
+	//bind the events to the dom nodes
+	function bindEvents(node) {
+		$(node).click(function() {
+    		$(this).toggleClass("complete").toggleClass("incomplete");
+    		$(this).children("#action").toggleClass("change-status").toggleClass("complete-icon");
+		});
+	}
+
+    //binds 
+    function trashMouseOn(node){
+        $(node).mouseenter(function(){
+            $('.complete').addClass('delete');
+            $('.complete-icon').toggleClass('delete-icon');
+        });
+    }
+
+     function trashMouseOff(node){
+        $(node).mouseleave(function(){
+            $('.complete').removeClass('delete');
+            $('.complete-icon').toggleClass('delete-icon');
+        });
+    }
+
+    //clears entire ul
+    function refreshList (){
+	   $('.list').empty();
+    }
+
+    //function to delete items on list that have been completed
+    function deleteComplete() {
+        $(this).closest('.page-wrap').find('.list').children('.delete').slideUp(function() {
+    	   $(this).remove();
+        });
+    }
+
+    //******Event Handlers******//
+
+    bindEvents($('.item-container'));
+    trashMouseOn($('#trash'));
+    trashMouseOff($('#trash'));
+    $('#user-input').keydown(addTodo);
+    $('.submit').click(addTodo);
+    $('#refresh').click(refreshList);
+    $('.list').sortable({
+    	axis: "y" 
     });
-}
-
-
-//******Event Handlers******//
-$('#user-input').keydown(enterSubmit);
-$('.submit').click(buttonSubmit);
-$('#refresh').click(refreshList);
-$('.list').sortable({ axis: "y" });
-$('#trash').click(deleteComplete);
+    $('#trash').click(deleteComplete);
 });
+
+
+
+
+
+
+
+
+
+
+
 
 
